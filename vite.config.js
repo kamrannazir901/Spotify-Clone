716ee557e741adb90d13b2ae2175ab7a574875bc
Kamran Nazir <kamrannazir901@gmail.com>
2025-06-08 09:34:57 +0500

    Fix routing structure and update article paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Spotify-Clone",
+  base: "/spotify-clone/",
 
   plugins: [react(), tailwindcss()],
 });
